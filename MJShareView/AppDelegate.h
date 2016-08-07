d64dcea459850df7e1094630c51b3a7ba9490a59
lmj <lmj@promote.cache-dns.local>
2016-08-07 12:34:13 +0000

    Initial Commit

---
+++ MJShareView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJShareView
+//
+//  Created by lmj on 16/8/7.
+//  Copyright © 2016年 MInJIng_Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJShareView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MJShareView
+//
+//  Created by lmj on 16/8/7.
+//  Copyright © 2016年 MInJIng_Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
